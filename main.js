f19b761c12ee4002721605fa34433008a732819d
martijnd <martijn.dorsman@gmail.com>
2016-04-09 16:44:35 +0200

    hide en show animaties

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	changeColor();
 	menu();
 	logo();
+	hide();
 });
 
 function resize(){
@@ -10,66 +11,162 @@ function resize(){
 	$(document).width($(window).width());
 };
 
+function hide(){
+	$("#blok1").hide();
+	$("#blok2").hide();
+	$("#blok3").hide();
+	$("#blok4").hide();
+	$("#blok5").hide();
+	$("#blok6").hide();
+	$("#blok7").hide();
+	$("#blok8").hide();
+	$("#blok9").hide();
+	$("#blok10").hide();
+	$("#blok11").hide();
+	$("#blok12").hide();
+	$("#blok13").hide();
+	$("#blok14").hide();
+	$("#blok15").hide();
+
+}
+
 function menu(){
 	$("#hamburgerwrap").click(function(){
 		$("#top").toggleClass("crossleft"),
 		$("#middle").toggleClass("displaynone"),
 		$("#bottom").toggleClass("crossright"),
 		$("#nav").toggleClass("navclicked"),
-		$("#titel").toggleClass("titelclicked")
-	})
-}
+		$("#titel").toggleClass("titelclicked"),
+
+		$("#blok1").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();
+		
+		$("#blok2").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();
+		
+		$("#blok3").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();
+		
+		$("#blok4").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();
+		
+		$("#blok5").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();
+
+		$("#blok6").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();
+
+		$("#blok7").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();	
+		
+		$("#blok8").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();
+
+		$("#blok9").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();
+		
+		$("#blok10").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();
+		
+		$("#blok11").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();
+		
+		$("#blok12").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();
+
+		$("#blok13").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();
+		
+		$("#blok14").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();
+		
+		$("#blok15").delay(250).queue(function(){
+			$(this).fadeToggle(500, "linear");
+			$(this).dequeue();
+								
+		});
+		});
+		});
+		});
+		});
+		});
+		});
+		});
+		});
+		});
+		});
+		});
+		});
+		});
+		});
+		});
+		}
+
 
 function logo(){
-	$("#logo").hover(function(){
+	$("#img").hover(function(){
 		$("#img").toggleClass("imghover")
 	})
 }
 
 function changeColor(){
-	$("#blok1").hover(function(){
+	$("#blok1").click(function(){
 		$('body').css("background-color", $(this).css('background-color'))
 	}),
-	$("#blok2").hover(function(){
+	$("#blok2").click(function(){
 		$('body').css("background-color", $(this).css('background-color'))
 	}),
-	$("#blok3").hover(function(){
+	$("#blok3").click(function(){
 		$('body').css("background-color", $(this).css('background-color'))
 	}),
-	$("#blok4").hover(function(){
+	$("#blok4").click(function(){
 		$('body').css("background-color", $(this).css('background-color'))
 	}),
-	$("#blok5").hover(function(){
+	$("#blok5").click(function(){
 		$('body').css("background-color", $(this).css('background-color'))
 	}),
-	$("#blok6").hover(function(){
+	$("#blok6").click(function(){
 		$('body').css("background-color", $(this).css('background-color'))
 	}),
-	$("#blok7").hover(function(){
+	$("#blok7").click(function(){
 		$('body').css("background-color", $(this).css('background-color'))
 	}),
-	$("#blok8").hover(function(){
+	$("#blok8").click(function(){
 		$('body').css("background-color", $(this).css('background-color'))
 	}),
-	$("#blok9").hover(function(){
+	$("#blok9").click(function(){
 		$('body').css("background-color", $(this).css('background-color'))
 	}),
-	$("#blok10").hover(function(){
+	$("#blok10").click(function(){
 		$('body').css("background-color", $(this).css('background-color'))
 	}),
-	$("#blok11").hover(function(){
+	$("#blok11").click(function(){
 		$('body').css("background-color", $(this).css('background-color'))
 	}),
-	$("#blok12").hover(function(){
+	$("#blok12").click(function(){
 		$('body').css("background-color", $(this).css('background-color'))
 	}),
-	$("#blok13").hover(function(){
+	$("#blok13").click(function(){
 		$('body').css("background-color", $(this).css('background-color'))
 	}),
-	$("#blok14").hover(function(){
+	$("#blok14").click(function(){
 		$('body').css("background-color", $(this).css('background-color'))
 	}),
-	$("#blok15").hover(function(){
+	$("#blok15").click(function(){
 		$('body').css("background-color", $(this).css('background-color')),$('#hilda').css("opacity", "1")
 	})
 
